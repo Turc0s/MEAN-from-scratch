084a45ae8f6e1f036735b630ffd3c0e4278d7006
tapsa <tapsa872@gmail.com>
2018-02-13 15:27:26 +0200

    Added service to connect contacts

---
+++ MEAN-app/src/app/contact.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import { Contact } from "./contact";
+import "rxjs/Rx";
+
+@Injectable()
+export class ContactService {
+
+  constructor(private _http: Http) { }
+
+  // retrieving ContactService
+  getContacts() {
+    return this._http.get("http://localhost:3000/api/contacts")
+            .map(res => res.json());
+  }
+
+  // add contact method
+  addContact(newContact) {
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    return this._http.post("http://localhost:3000/api/contact", newContact, {headers: headers})
+                  .map(res => res.json());
+  }
+
+  // delete contact
+  deleteContact(id) {
+    return this._http.delete("http://localhost:3000/api/contact"+id)
+                .map(res => res.json());
+
+  }
+
+
+}
---
+++ MEAN-app/src/app/contacts/contacts.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
+import { Contact } from "../contact";
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css'],
+  providers:[ContactService]
+})
+export class ContactsComponent implements OnInit {
+
+  contacts: Contact[];
+  contact: Contact;
+  first_name: string;
+  last_name: string;
+  phone: number;
+
+  constructor(private _contactService: ContactService) { }
+
+  ngOnInit() {
+    this._contactService.getContacts()
+              .subscribe(contacts => {
+                this.contacts = contacts;
+              });
+  }
+
+
+
+}
