c262637a88e29fd4e5c2e774b35bdb707c0aed72
tapsa <tapsa872@gmail.com>
2018-02-13 14:51:56 +0200

    Added mongodb functionality to save data to local db

--- MEAN-app/routes/route.js
+++ MEAN-app/routes/route.js
@@ -2,20 +2,44 @@
 const express = require("express");
 const router = express.Router();
 
+const Contact = require("../models/contact");
 
 // retrieving contacts
 router.get("/contacts",  (req, res, next) => {
-    res.send("Retrieving the contact list");
+    // res.send("Retrieving the contact list");
+    Contact.find((err, contacts) => {
+        res.json(contacts);
+    });
 });
 
 // add contact
 router.post("/contact", (req, res, next) => {
     // logic to add contact
+    let newContact = new Contact({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone: req.body.phone
+    });
+
+    newContact.save((err, contact) => {
+        if(err) {
+            res.json({msg: "Failed to add contact"});
+        } else {
+            res.json({msg: "Contact successfully added"});
+        }
+    });
 }); 
 
 // delete contacts
 router.delete("/contact/:id", (req, res, next) => {
     // logic to delete contact
+    Contact.remove({_id: req.params.id}, (err, result) => {
+        if(err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
 });
 
 module.exports = router;
